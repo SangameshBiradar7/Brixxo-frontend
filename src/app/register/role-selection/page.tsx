d4e63c988d40fac681cce7b31ea9e0242573e128
SangameshBiradar7 <sangameshbiradar911@gmail.com>
2025-12-10 10:11:16 +0530

    login page

--- src/app/register/role-selection/page.tsx
+++ src/app/register/role-selection/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 import Link from 'next/link';
 import Navigation from '@/components/Navigation';
 
